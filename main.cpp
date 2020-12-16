85237df7102c0e95ac11c2dce83dd2ffb628303a
GabrielBronte <gabrielbronte@ice.ufjf.br>
2020-12-16 16:56:52 -0300

    le Arquivo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <cstdlib>
 #include <chrono>
@@ -9,6 +10,17 @@
 using namespace std;
 using namespace std::chrono;
 
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
 
 void leArquivoTextoGeral(ifstream& arq)
 {
@@ -17,41 +29,33 @@ void leArquivoTextoGeral(ifstream& arq)
     if(arq.is_open())
     {
         string str;
-        string date;
-        string state;
-        string name;
-        string code;
-        string casesString;
-        string deathsString;
-        int cases;
-        int deaths;
+        int cases, deaths;
 
-        for(int i = 0; i < 10000; i++){
-        Registro *registra = new Registro();
-        
-        getline(arq, date,',');
-        getline(arq, state,',');
-        getline(arq, name,',');
-        getline(arq, code,',');
-        getline(arq, casesString,',');
-        getline(arq, deathsString);
+        for(int i = 0; getline(arq,str); i++){
+            if(i!=0){
+                Registro *registra = new Registro();
 
-        cases = atoi(casesString.c_str());
-        deaths = atoi(deathsString.c_str());
+                vector<string> teste = split(str,',');
 
-        registra->setDate(date);
-        registra->setState(state);
-        registra->setName(name);
-        registra->setCode(code);
-        registra->setCases(cases);
-        registra->setDeaths(deaths);
+                cases = atoi(teste[4].c_str());
+                deaths = atoi(teste[5].c_str());
 
-        registros.push_back(*registra);
-        cout << registros[i].getCases()<< endl;
+                registra->setDate(teste[0]);
+                registra->setState(teste[1]);
+                registra->setName(teste[2]);
+                registra->setCode(teste[3]);
+                registra->setCases(cases);
+                registra->setDeaths(deaths);
+
+
+                registros.push_back(*registra);
+            }
         }
     }
+    
     else
         cerr << "ERRO: O arquivo nao pode ser aberto!" << endl;
+    
 }
 
 int main(int argc, char const *argv[]){
